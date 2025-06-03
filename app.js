4637694fcaeb01df3b8aa347a1f29181ecdb0730
Anisul Islam <anis.inbox10@gmail.com>
2025-06-04 03:08:42 +0500

    dd

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
 const { readdirSync } = require('fs');
+const serverless = require('serverless-http'); // ✅ Import this
+
 require('dotenv').config();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 // Load all routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
-db();
 
-module.exports = serverless(app);
+db(); // ✅ DB connected
+
+module.exports = serverless(app); // ✅ Wrap and export
